968a9938fa2608ebba45e0b134036da545017c4d
aakansha16mp2it <aakansha16.mp2it.mtr@gmail.com>
2022-02-01 11:58:01 +0530

    last and final update

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,7 +18,8 @@ class UserSeeder extends Seeder
             'role' => 'admin',
             'email' =>'aakansha@gmail.com',
             'phone' => 98674746,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'email_verified' => '2022-01-31 17:38:18' // password
         ]);
     }
 }
